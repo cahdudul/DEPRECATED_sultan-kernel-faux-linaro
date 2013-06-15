14a21848ff2fafe97cb45ac1f3e27c186343837c
sultanxda <sultanxda@gmail.com>
2013-06-14 21:47:14 -0700

    Revert changes to battey driver

--- arch/arm/mach-msm/include/mach/htc_battery_common.h
+++ arch/arm/mach-msm/include/mach/htc_battery_common.h
@@ -43,10 +43,6 @@ enum charger_control_flag {
 	ENABLE_CHARGER,
 	ENABLE_LIMIT_CHARGER,
 	DISABLE_LIMIT_CHARGER,
-#if defined(CONFIG_MACH_PRIMODD) || defined(CONFIG_MACH_PRIMOTD)
-	PHONE_CALL_IN,
-	PHONE_CALL_STOP,
-#endif
 	END_CHARGER
 };
 /* context event */
